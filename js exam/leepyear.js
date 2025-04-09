cc8d2e0a847048ba20fedcc8ed75cce13e22bc32
Dishant G  Kanak <dishantkanakpvt@gmail.com>
2025-04-09 14:40:03 +0530

    Add files via upload

---
+++ js exam/leepyear.js
@@ -0,0 +1,11 @@
+// Leep Year 
+
+let year=parseInt(prompt("Enter Your Year :"));
+
+if(year % 4 == 0){
+    console.log("That Year Was Leep Year");
+}
+else{
+    
+    console.log("That Year Was Not Leep Year");
+}
